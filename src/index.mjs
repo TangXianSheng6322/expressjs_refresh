8d302a9aaffda3ce8e26990540f9eaae80c0f4b9
Nick <n.sakharov@icloud.com>
2024-10-03 13:45:32 +0700

    Route Parameters

---
+++ src/index.mjs
@@ -0,0 +1,33 @@
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const mockUsers = [
+  { id: 1, username: "violet", displayName: "SLQueen" },
+  { id: 2, username: "sasha", displayName: "kOL" },
+  { id: 3, username: "nina", displayName: "Joomn" },
+  { id: 4, username: "mike", displayName: "TYSM" },
+];
+
+app.get("/", (req, res) => {
+  res.send({ msg: "Hello" });
+});
+
+app.get("/api/user", (req, res) => {
+  res.status(201).send(mockUsers);
+});
+
+app.get("/api/user/:id", (req, res) => {
+  const parsedId = parseInt(req.params.id);
+  console.log(parsedId);
+  if (isNaN(parsedId))
+    return res.status(400).send({ msg: "Bad Request. Invalid Id" });
+  const findUser = mockUsers.find((user) => user.id === parsedId);
+  if (!findUser) return res.sendStatus(404);
+  return res.send(findUser);
+});
+
+app.listen(PORT, () => {
+  console.log(`Port is running on ${PORT}`);
+});
