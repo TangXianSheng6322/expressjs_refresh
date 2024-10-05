ce2b5c764d09d6f7f44abfa069045f1e0e71656c
Nick <n.sakharov@icloud.com>
2024-10-05 20:03:12 +0700

    Post requests

--- src/index.mjs
+++ src/index.mjs
@@ -1,6 +1,7 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
 const mockUsers = [
@@ -23,6 +24,15 @@ app.get("/api/user", (req, res) => {
     res.send(mockUsers.filter((user) => user[filter].includes(value)));
   }
   res.status(201).send(mockUsers);
+});
+
+app.post("/api/user", (req, res) => {
+  console.log(req.body);
+  const { body } = req;
+
+  const newUser = { id: mockUsers[mockUsers.length - 1].id + 1, ...body };
+  mockUsers.push(newUser);
+  return res.status(201).send(newUser);
 });
 
 app.get("/api/user/:id", (req, res) => {
