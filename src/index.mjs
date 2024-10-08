29e0a19e2a9acc9238b5c79c9719a6cbee07600a
Nick <n.sakharov@icloud.com>
2024-10-08 22:21:31 +0700

    Express validator npm package test

--- src/index.mjs
+++ src/index.mjs
@@ -1,4 +1,12 @@
 import express from "express";
+import {
+  query,
+  validationResult,
+  body,
+  matchedData,
+  checkSchema,
+} from "express-validator";
+import { createValidationSchemas } from "./utils/validationSchemas.mjs";
 
 const app = express();
 app.use(express.json());
@@ -35,26 +43,56 @@ app.get("/", loggingMiddlewear, (req, res) => {
   res.send({ msg: "Hello" });
 });
 
-app.get("/api/user", loggingMiddlewear, (req, res) => {
-  // const { filter, value } = req.query;
-  const {
-    query: { filter, value },
-  } = req;
-  if (filter && value) {
-    return res.send(mockUsers.filter((user) => user[filter].includes(value)));
-  } else {
-    return res.status(200).send(mockUsers);
+app.get(
+  "/api/user",
+  query("filter")
+    .isString()
+    .notEmpty()
+    .withMessage("Must not be empty")
+    .isLength({ min: 3, max: 4 })
+    .withMessage(
+      "Must should contain the minimum of 5 characters and the maximun of 10 characters"
+    ),
+  loggingMiddlewear,
+  (req, res) => {
+    // const { filter, value } = req.query;
+    const {
+      query: { filter, value },
+    } = req;
+    console.log(validationResult(req));
+    if (filter && value) {
+      return res.send(mockUsers.filter((user) => user[filter].includes(value)));
+    } else {
+      return res.status(200).send(mockUsers);
+    }
   }
-});
+);
 
-app.post("/api/user", (req, res) => {
-  console.log(req.body);
-  const { body } = req;
+app.post(
+  "/api/user",
+  // [
+  //   body("userName")
+  //     .notEmpty()
+  //     .withMessage("Username cannot be empty")
+  //     .isLength({ min: 5, max: 30 })
+  //     .withMessage("Username should contain 5-30 characters")
+  //     .isString()
+  //     .withMessage("Username must be a string"),
+  //   body("displayName").notEmpty(),
+  // ],
+  checkSchema(createValidationSchemas),
+  (req, res) => {
+    console.log(req.body);
+    if (!validationResult(req).isEmpty()) {
+      return res.status(400).send({ errors: validationResult(req).array() });
+    }
 
-  const newUser = { id: mockUsers[mockUsers.length - 1].id + 1, ...body };
-  mockUsers.push(newUser);
-  return res.status(201).send(newUser);
-});
+    const data = matchedData(req);
+    const newUser = { id: mockUsers[mockUsers.length - 1].id + 1, ...data };
+    mockUsers.push(newUser);
+    return res.status(201).send(newUser);
+  }
+);
 
 app.get("/api/user/:id", resolveUserById, (req, res) => {
   const { findIndex } = req;
